0373605401aeb40ce15055e5179ecac94d29e5e1
Zev Weiss <zev@bewilderbeest.net>
2022-12-13 12:52:12 +0000

    sysfs: Add doc comments

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -1,3 +1,5 @@
+//! Utility code for interacting with sysfs files.
+
 use tokio::io::{AsyncReadExt, AsyncSeekExt};
 
 use crate::{
@@ -5,6 +7,10 @@ use crate::{
 	types::*,
 };
 
+/// Read a bit of data from `fd` (at offset zero) and parse it as a `T`.
+///
+/// (While not strictly required, the expectation is that `fd` refers to a sysfs file that
+/// contains a single integer.)
 pub async fn read_and_parse<T: std::str::FromStr>(fd: &mut tokio::fs::File) -> ErrResult<T> {
 	let mut buf = [0u8; 128];
 
@@ -20,9 +26,9 @@ pub async fn read_and_parse<T: std::str::FromStr>(fd: &mut tokio::fs::File) -> E
 		.map_err(|_| err_invalid_data(format!("invalid sysfs data: {}", s)))
 }
 
-// Convenience function for cases where we expect a glob to produce
-// exactly one match.  Returns Ok(_) if so, and Err(_) on multiple
-// matches, no matches, or other errors.
+/// Convenience function for cases where we expect a glob to produce exactly one match.
+///
+/// Returns Ok(_) if so, and Err(_) on multiple matches, no matches, or other errors.
 pub fn get_single_glob_match(pattern: &str) -> ErrResult<std::path::PathBuf> {
 	let mut matches = glob::glob(&pattern)?;
 	let first = match matches.next() {
@@ -36,30 +42,36 @@ pub fn get_single_glob_match(pattern: &str) -> ErrResult<std::path::PathBuf> {
 	}
 }
 
-// For cases where we expect exactly one .../hwmon/hwmonX subdirectory of a
-// given path, this finds the one that's there, ensures there aren't any others,
-// and returns it (including whatever original prefix was passed).
+/// For cases where we expect exactly one `.../hwmon/hwmonX` subdirectory of a given path.
+///
+/// This finds the one that's there, ensures there aren't any others, and returns it
+/// (including whatever original prefix was passed).
 pub fn get_single_hwmon_dir(path: &str) -> ErrResult<std::path::PathBuf> {
 	let pattern = format!("{}/hwmon/hwmon[0-9]*", path);
 	get_single_glob_match(&pattern)
 }
 
-// Summary info about a hwmon *_input file
+/// Summary info about a hwmon `*_input` file
 pub struct HwmonFileInfo {
+	/// The full absolute path of the file.
 	pub abspath: std::path::PathBuf,
 
-	// Filename with  "_input" stripped off, e.g. "in1", "temp3", etc.
-	// Allocation here is unfortunate, but AFAIK we can't borrow from
-	// abspath without messing around with Pin and such.
+	/// The filename with `"_input"` stripped off, e.g. `"in1"`, `"temp3"`, etc.
+	///
+	/// The separate allocation here is unfortunate, but AFAIK we can't borrow from
+	/// `abspath` without messing around with [`Pin`](std::pin::Pin) and such.
 	pub base: String,
 
+	/// The type of sensor the file reads from.
 	pub kind: SensorType,
 
-	// Just the numeric part of base, parsed out (mostly just for sorting)
+	/// Just the numeric part of `base`, parsed out (mostly just for sorting).
 	pub idx: usize,
 }
 
 impl HwmonFileInfo {
+	/// Construct a [`HwmonFileInfo`] from the given path.
+	// ...though we don't actually check that it's absolute...should we?
 	pub fn from_abspath(abspath: std::path::PathBuf) -> ErrResult<Self> {
 		let mk_err = |msg| {
 			err_invalid_data(format!("{}: {}", abspath.display(), msg))
@@ -93,6 +105,10 @@ impl HwmonFileInfo {
 		})
 	}
 
+	/// Return the label associated with the file.
+	///
+	/// If a corresponding `*_label` file exists, this is obtained by reading from it;
+	/// otherwise it's just `self`'s [`base`](HwmonFileInfo::base) member.
 	pub fn get_label(&self) -> ErrResult<String> {
 		let labelpath = self.abspath.with_file_name(format!("{}_label", self.base));
 		if labelpath.is_file() {
@@ -103,8 +119,12 @@ impl HwmonFileInfo {
 	}
 }
 
-// fileprefix could just be a &str (with "" instead of None), but
-// meh...might as well make it slightly more explicit I guess?
+/// Scan a device directoy fro hwmon `*_input` files.
+///
+/// Look in `devdir` for hwmon `*_input` files, optionally filtered to just those whose
+/// names start with a certain prefix if `fileprefix` is [`Some`].
+// fileprefix could just be a &str (with "" instead of None), but we might as well make
+// it slightly more explicit.
 pub fn scan_hwmon_input_files(devdir: &std::path::Path, fileprefix: Option<&str>) -> ErrResult<Vec<HwmonFileInfo>> {
 	let hwmondir = get_single_hwmon_dir(&devdir.to_string_lossy())?;
 	let pattern = hwmondir.join(format!("{}*_input", fileprefix.unwrap_or("")));
@@ -132,12 +152,21 @@ pub fn scan_hwmon_input_files(devdir: &std::path::Path, fileprefix: Option<&str>
 	Ok(info)
 }
 
+/// Information for reading sensor data from sysfs.
 pub struct SysfsSensorIO {
+	/// A file descriptor to (asynchronously) read from.
 	fd: tokio::fs::File,
+
+	/// A multiplicative scale factor to convert raw sysfs file data to the desired
+	/// units.
+	///
+	/// For example, the hwmon subsystem provides temperature readings in millidegrees
+	/// C, so 0.001 here would convert that to degrees C as desired elsewhere.
 	scale: f64,
 }
 
 impl SysfsSensorIO {
+	/// Construct a [`SensorIO`] for a provided hwmon file.
 	pub async fn new(file: &HwmonFileInfo) -> ErrResult<SensorIO> {
 		let fd = tokio::fs::File::open(&file.abspath).await?;
 		Ok(SensorIO::Sysfs(SysfsSensorIO {
@@ -146,6 +175,8 @@ impl SysfsSensorIO {
 		}))
 	}
 
+	/// Read a sensor sample (in natural units, e.g. degrees C instead of hwmon's
+	/// millidegrees C).
 	pub async fn read(&mut self) -> ErrResult<f64> {
 		let ival = read_and_parse::<i32>(&mut self.fd).await?;
 		Ok((ival as f64) * self.scale)

2d98e3bbfa2c549ec056d5dfded29d5849934f4d
Zev Weiss <zev@bewilderbeest.net>
2022-12-13 11:20:56 +0000

    powerstate: Add doc comments

--- src/powerstate.rs
+++ src/powerstate.rs
@@ -1,3 +1,5 @@
+//! Utility code for monitoring host power state transitions.
+
 use std::{
 	ops::DerefMut,
 	sync::Mutex as SyncMutex,
@@ -10,15 +12,22 @@ use dbus::{
 
 use crate::types::*;
 
+/// Represents a setting of a sensor indicating in which host power states it is enabled.
 #[derive(Debug, Copy, Clone)]
 pub enum PowerState {
+	/// Sensor enabled any time the host is powered on.
 	On,
+	/// Sensor enabled When the host is powered on and has completed its BIOS POST phase.
 	BiosPost,
+	/// Sensor always enabled, regardless of host state.
 	Always,
+	/// Sensor enabled when host chassis power is on.
 	ChassisOn,
 }
 
 impl PowerState {
+	/// Test if a sensor whose power state is `self` should currently be enabled,
+	/// based on our tracking of the host's current power state.
 	pub fn active_now(&self) -> bool {
 		let host = HOST_STATE.lock().unwrap();
 		match self {
@@ -43,30 +52,49 @@ impl TryFrom<&str> for PowerState {
 	}
 }
 
+/// Represents various attributes of a host system's power state.
 pub struct HostState {
+	/// Whether or not chassis power is on.
 	pub chassis_on: bool,
+	/// Whether or not host system power is on.
 	pub power_on: bool,
+	/// Whether or not the host system has completed its POST phase.
 	pub post_complete: bool,
 }
 
+/// The current power state of the host.
 static HOST_STATE: SyncMutex<HostState> = SyncMutex::new(HostState {
 	chassis_on: false,
 	power_on: false,
 	post_complete: false,
 });
 
+/// A collection of information pertaining to the dbus representation of an attribute of
+/// host power state.
 pub struct DBusPowerStateProperty {
+	/// The bus name of the service from which we obtain the current state.
 	pub busname: &'static str,
+	/// The path of the object we retrieve the state from.
 	pub path: &'static str,
+	/// The interface of the object via which we retrieve the state.
 	pub interface: &'static str,
+	/// The specific property within the interface that we query to retrieve the state.
 	pub property: &'static str,
 
+	/// Which [`PowerState`] this property pertains to.  The datatype here is a slight
+	/// kludge in that [`PowerState`] is mostly intended to represent a setting of a
+	/// sensor rather than something about the host, but it's a close-enough fit that
+	/// I'm reusing it at least for now...
 	pub power_state: PowerState,
+
+	/// How to map the string retrieved from dbus to the on/off state of the attribute.
 	pub is_active: fn(s: &str) -> bool,
+	/// How to update this property within a [`HostState`].
 	pub update: fn(st: &mut HostState, new: bool),
 }
 
 impl DBusPowerStateProperty {
+	/// Retrieve the current state of the property represented by `self` from dbus.
 	pub async fn get(&self, bus: &nonblock::SyncConnection) -> ErrResult<bool> {
 		let p = nonblock::Proxy::new(self.busname, self.path,
 					     std::time::Duration::from_secs(30), bus);
@@ -77,8 +105,10 @@ impl DBusPowerStateProperty {
 }
 
 mod dbus_properties {
+	//! A small collection of static [`DBusPowerStateProperty`] instances.
 	use super::{PowerState, DBusPowerStateProperty};
 
+	/// Host chassis power state.
 	pub const CHASSIS: DBusPowerStateProperty = DBusPowerStateProperty {
 		busname: "xyz.openbmc_project.State.Chassis",
 		path: "/xyz/openbmc_project/state/chassis0",
@@ -90,6 +120,7 @@ mod dbus_properties {
 		update: |s, n| { s.chassis_on = n; }
 	};
 
+	/// Host system power state.
 	pub const POWER: DBusPowerStateProperty = DBusPowerStateProperty {
 		busname: "xyz.openbmc_project.State.Host",
 		path: "/xyz/openbmc_project/state/host0",
@@ -101,6 +132,7 @@ mod dbus_properties {
 		update: |s, n| { s.power_on = n; }
 	};
 
+	/// Host POST state.
 	pub const POST: DBusPowerStateProperty = DBusPowerStateProperty {
 		busname: "xyz.openbmc_project.State.OperatingSystem",
 		path: "/xyz/openbmc_project/state/os",
@@ -113,6 +145,7 @@ mod dbus_properties {
 	};
 }
 
+/// Initialize [`HOST_STATE`] from dbus.
 pub async fn init_host_state(bus: &nonblock::SyncConnection) {
 	use dbus_properties::*;
 
@@ -123,7 +156,7 @@ pub async fn init_host_state(bus: &nonblock::SyncConnection) {
 	let retrieve = |res: ErrResult<bool>, name| {
 		res.unwrap_or_else(|e| {
 			eprintln!("Failed to retrieve {} status from dbus: {}", name, e);
-			false
+			false // on error, assume things are off.
 		})
 	};
 
@@ -138,6 +171,11 @@ pub async fn init_host_state(bus: &nonblock::SyncConnection) {
 	state.post_complete = post;
 }
 
+/// Register a callback `cb` to called on host power state transitions.
+///
+/// `cb` will be called with the attribute of power state that changed (again kind of a
+/// hack datatype-wise, see [`DBusPowerStateProperty::power_state`]) and the new state of
+/// that attribute (`true` for on, `false` for off).
 pub async fn register_power_signal_handler<F, R>(bus: &nonblock::SyncConnection, cb: F) -> ErrResult<Vec<nonblock::MsgMatch>>
 	where F: FnOnce(PowerState, bool) -> R + Send + Copy + Sync + 'static,
 	      R: futures::Future<Output = ()> + Send
